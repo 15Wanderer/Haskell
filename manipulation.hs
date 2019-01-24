9847e7dcf8ebc8cb0add4032d3770872e0404d52
Kyle Jarrett <kyle@jarrett.org>
2019-01-23 20:23:15 -0500

    Merged

--- manipulation.hs
+++ manipulation.hs
@@ -1,6 +1,5 @@
 import Data.List
 
-
 isEven str n = if (mod n 2) == 0
 
 		then str
@@ -9,13 +8,20 @@ isEven str n = if (mod n 2) == 0
 
 
 
-
 alternate strs = [ oddFlip a b | (a, b) <- zip strs [1..]]
-
-
 
 oddFlip as b = if even b
 
 		then as
 
 		else reverse as
+
+chunker strs = if (length strs) > 20
+
+                then ([] ++ (take 20 strs)) ++ chunker (drop 20 strs)
+
+                else [] ++ strs
+
+
+
+add a b = a + b
